7c309edef00ce284bf2e69fa2c095bda1fe6a479
clmcclease <chris@mcclease.dev>
2022-01-10 14:33:07 -0600

    I think I got security configured correctly....

---
+++ src/main/java/dev/mcclease/loginbackend/registration/RegistrationService.java
@@ -0,0 +1,10 @@
+package dev.mcclease.loginbackend.registration;
+
+
+
+public class RegistrationService {
+
+    public String register(RegistrationRequest request) {
+        return "works";
+    }
+}
