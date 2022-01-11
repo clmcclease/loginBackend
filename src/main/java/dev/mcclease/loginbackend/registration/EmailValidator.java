5fb923f7741c5aaf69baba97f45104e71bec6e50
clmcclease <chris@mcclease.dev>
2022-01-11 00:01:12 -0600

    going to bed.

--- src/main/java/dev/mcclease/loginbackend/registration/EmailValidator.java
+++ src/main/java/dev/mcclease/loginbackend/registration/EmailValidator.java
@@ -1,11 +1,15 @@
 package dev.mcclease.loginbackend.registration;
 
 import java.util.function.Predicate;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailValidator implements Predicate<String> {
     @Override
     public boolean test(String s) {
         //make a regex to validate.
         return true;
     }
+
+
 }
